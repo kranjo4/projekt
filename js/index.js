335020e7d63ccd5baee05b74cabc46e876950343
RokKrajnc <rok.krajnc5@student.um.si>
2024-05-02 16:21:23 +0200

    prvi commit

---
+++ js/index.js
@@ -0,0 +1,69 @@
+const dropArea = document.getElementById('drop-area');
+const resultDiv = document.getElementById('result');
+const fileInput = document.getElementById('fileElem');
+const fileButton = document.getElementById('fileButton');
+const uploadButton = document.getElementById('uploadButton');
+
+dropArea.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dropArea.classList.add('highlight');
+});
+
+dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('highlight');
+});
+
+dropArea.addEventListener('drop', (event) => {
+    event.preventDefault();
+    dropArea.classList.remove('highlight');
+    const files = event.dataTransfer.files;
+    handleFiles(files);
+});
+
+fileInput.addEventListener('change', () => {
+    const files = fileInput.files;
+    handleFiles(files);
+});
+
+fileButton.addEventListener('click', () => {
+    fileInput.click();
+});
+
+function handleFiles(files) {
+    const file = files[0];
+    if (file) {
+        uploadButton.style.display = 'block';
+        fileNameDisplay.innerText = file.name;
+    } else {
+        uploadButton.style.display = 'none';
+        fileNameDisplay.innerText = '';
+    }
+}
+
+uploadButton.addEventListener('click', () => {
+    const files = fileInput.files;
+    handleUpload(files);
+});
+
+function handleUpload(files) {
+    const file = files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('https://snyk.io/api/v1/test', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Authorization': 'Token c7d9e33b-91e6-427f-b7b9-daaa927d918f'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        resultDiv.innerHTML = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        console.error('Napaka pri pošiljanju zahteve:', error);
+        resultDiv.innerHTML = 'Napaka pri pošiljanju zahteve';
+    });
+}
+
